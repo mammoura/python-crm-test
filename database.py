de48e46043647fefdf10d4df6ba45533df3eb9e8
Mohammad A <m.ammoura@gmail.com>
2021-12-06 14:42:17 +0100

    back to json db

--- database.py
+++ database.py
@@ -22,14 +22,14 @@ class Database:
 
         #Open the spicified database file for reading and performaing loading
         with open(path, "r") as handle:
-            #import json
-            #self.data = json.load(handle)
+            import json
+            self.data = json.load(handle)
 
             #import  yaml
             #self.data = yaml.safe_load(handle)
 
-            import xmltodict
-            self.data = xmltodict.parse(handle.read())["root"]
+            #import xmltodict
+            #self.data = xmltodict.parse(handle.read())["root"]
             print(self.data)
 
 
